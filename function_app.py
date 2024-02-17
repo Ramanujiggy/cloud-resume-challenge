ecbde590359dab53dd29f56a879be7262614d4da
ramanujiggy <robertdgelin@gmail.com>
2024-02-17 00:04:09 -0600

    trying to connect function to cosmosdb

--- function_app.py
+++ function_app.py
@@ -1,19 +1,17 @@
 import azure.functions as func
 import logging
-from azure.cosmos import CosmosClient
+from azure.cosmos import CosmosClient, exceptions
+import json 
+
+URL="https://sitedb-2.documents.azure.com:443/"
+KEY="Hsv5c61KdKUX5vybw3nDvrTKjy6uJFeIBlwCBaoEIRvEbhnwUgvSqScvF9VZBsUi68quI1sxcm4uACDbudW4WQ=="
 
 
-
-#Cosmos DB Endpoint
-COSMOSDB_ENDPOINT ="https://sitedb-2.documents.azure.com:443/"
-COSMOSDB_KEY="Hsv5c61KdKUX5vybw3nDvrTKjy6uJFeIBlwCBaoEIRvEbhnwUgvSqScvF9VZBsUi68quI1sxcm4uACDbudW4WQ=="
-DATABASE_NAME = ""
-CONTAINER_NAME=""
-
-
-client = CosmosClient(COSMOSDB_ENDPOINT, COSMOSDB_KEY)
+client=CosmosClient(URL,credential=KEY)
+DATABASE_NAME='ToDoList'
 database = client.get_database_client(DATABASE_NAME)
-container = database.get_container_client(CONTAINER_NAME)
+CONTAINER_NAME='Items'
+container=database.get_container_client(CONTAINER_NAME)
 
 
 
@@ -23,20 +21,11 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 @app.route(route="http_trigger")
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
+    try:
+        container.upsert_item(body={'id':'sitevisit', 'visits':1})
+        return func.HttpResponse("site visit tracked :]", status_code=200)
+    except exceptions.CosmosHttpResponseError :
+        return func.HttpResponse("Error: This didn't work")
+    
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+        
