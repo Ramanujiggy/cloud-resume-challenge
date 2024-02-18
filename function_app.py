b79b04aa3e1e01b344d430fedbb92f5be95bef5e
ramanujiggy <robertdgelin@gmail.com>
2024-02-18 17:19:32 -0600

    updating function_app.py

--- function_app.py
+++ function_app.py
@@ -1,31 +1,35 @@
 import azure.functions as func
 import logging
-from azure.cosmos import CosmosClient, exceptions
+from azure.cosmos import CosmosClient, exceptions, partition_key
 import json 
 
 URL="https://sitedb-2.documents.azure.com:443/"
 KEY="Hsv5c61KdKUX5vybw3nDvrTKjy6uJFeIBlwCBaoEIRvEbhnwUgvSqScvF9VZBsUi68quI1sxcm4uACDbudW4WQ=="
 
-
-client=CosmosClient(URL,credential=KEY)
 DATABASE_NAME='ToDoList'
-database = client.get_database_client(DATABASE_NAME)
 CONTAINER_NAME='Items'
-container=database.get_container_client(CONTAINER_NAME)
-
 
 
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
+
 @app.route(route="http_trigger")
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     try:
-        container.upsert_item(body={'id':'sitevisit', 'visits':1})
-        return func.HttpResponse("site visit tracked :]", status_code=200)
-    except exceptions.CosmosHttpResponseError :
-        return func.HttpResponse("Error: This didn't work")
-    
-
         
+        client=CosmosClient(URL,credential=KEY)
+        database = client.get_database_client(DATABASE_NAME)
+        container=database.get_container_client(CONTAINER_NAME)
+        
+        query='SELECT VALUE Items.visit  FROM Items'
+        
+        counter=container.read_item(query=query, enable_cross_partition_query=True)
+        
+        counter = counter + 1 
+        container.upsert_item(counter)
+        
+        return func.HttpResponse(f"site visit tracked this is visit{counter} :]", status_code=200)
+    except Exception as e:
+        return func.HttpResponse(f"error: {str(e)}")
