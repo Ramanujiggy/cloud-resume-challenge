81785bbc88d62c2ef79519b9ce302c83241df494
ramanujiggy <robertdgelin@gmail.com>
2024-02-20 11:33:40 -0600

    working on http trigger

--- function_app.py
+++ function_app.py
@@ -1,19 +1,13 @@
 import azure.functions as func
 import logging
-from azure.cosmos import CosmosClient, exceptions, partition_key
-import json 
+from azure.cosmos import CosmosClient
 
 URL="https://sitedb-2.documents.azure.com:443/"
 KEY="Hsv5c61KdKUX5vybw3nDvrTKjy6uJFeIBlwCBaoEIRvEbhnwUgvSqScvF9VZBsUi68quI1sxcm4uACDbudW4WQ=="
-
 DATABASE_NAME='ToDoList'
 CONTAINER_NAME='Items'
 
-
-
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
-
-
 @app.route(route="http_trigger")
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
@@ -23,13 +17,19 @@ def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
         database = client.get_database_client(DATABASE_NAME)
         container=database.get_container_client(CONTAINER_NAME)
         
-        query='SELECT VALUE Items.visit  FROM Items'
+        query='SELECT * FROM Items WHERE Items.id = "counter"'
         
-        counter=container.read_item(query=query, enable_cross_partition_query=True)
-        
-        counter = counter + 1 
-        container.upsert_item(counter)
-        
-        return func.HttpResponse(f"site visit tracked this is visit{counter} :]", status_code=200)
+        result=list(container.query_items(query=query, enable_cross_partition_query=True))
+        if result:
+            counter_item = result[0]
+            counter = counter_item['visit'] + 1
+            counter_item['visit']=counter
+            
+            
+            container.replace_item(item=counter_item body=counter)
+            
+            return func.HttpResponse(f"site visit tracked, this is visit {counter}:]")
+        else:
+            return func.HttpResponse("Counter item not found", status=404)    
     except Exception as e:
         return func.HttpResponse(f"error: {str(e)}")
