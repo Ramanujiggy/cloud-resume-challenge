fc0d2e59267b522fbef3ee3ac3327f5fb828cd4b
ramanujiggy <robertdgelin@gmail.com>
2024-03-18 06:30:36 -0500

    trying to make the azure function again

--- function_app.py
+++ function_app.py
@@ -1,23 +1,32 @@
+import os 
 import azure.functions as func
 import logging
 from azure.cosmos import CosmosClient, exceptions 
 
-URL="https://sitedb-2.documents.azure.com:443/"
-KEY="Hsv5c61KdKUX5vybw3nDvrTKjy6uJFeIBlwCBaoEIRvEbhnwUgvSqScvF9VZBsUi68quI1sxcm4uACDbudW4WQ=="
-DATABASE_NAME='ToDoList'
-CONTAINER_NAME='Items'
+url = os.environ["ACCOUNT_URI"]
+key = os.environ["ACCOUNT_KEY"]
+DATABASE_NAME= os.environ["DATABASE_NAME"]
+CONTAINER_NAME = os.environ["CONTAINER_NAME"]
 
 
-
-
+client=CosmosClient(url,key)
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
+
+
+
+@app.cosmos_db_input(arg_name="documents", 
+    database_name=os.environ["DATABASE_NAME"],
+    collection_name=os.environ["CONTAINER_NAME"],
+    id="1",
+    partition_key="/Visits",
+    connection_string_setting="AccountEndpoint=https://sitedb-2.documents.azure.com:443/;AccountKey=Hsv5c61KdKUX5vybw3nDvrTKjy6uJFeIBlwCBaoEIRvEbhnwUgvSqScvF9VZBsUi68quI1sxcm4uACDbudW4WQ==;")
+
 @app.route(route="http_trigger")
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     count = 0
     query= "SELECT * FROM c WHERE c.id = '1'"
     try:
-        client=CosmosClient(URL,credential=KEY)
         database = client.get_database_client(DATABASE_NAME)
         container=database.get_container_client(CONTAINER_NAME)
         
@@ -26,10 +35,10 @@ def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
             count = int(item['count'])
             count +=1 
             updated_item['count']=count
-            container.upsert_item(item, updated_item)
+            container.upsert_item(updated_item)
     
     except exceptions.CosmosHttpResponseError as e:
-        logging.error(f"An error occured: {e.status_code} +  {e.message}")
+        logging.error(f"An error occured: {e.status_code} + {e.message}")
         count = e.message
     
     return func.HttpResponse(body=str(count),status_code=200)
